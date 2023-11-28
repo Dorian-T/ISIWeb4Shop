cec5da4c2a3d684c8df0a70a4bcdccd60e7c699f
JulesUSG15 <jules.ginhac@etu.univ-lyon1.fr>
2023-11-28 23:25:52 +0100

    Page accueil: ok Page produits: ok sauf css

---
+++ controleurs/Home_controleur.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Home_controller {
+
+  private $home_modele;
+
+  public function __construct() {
+  }
+
+  public function afficherHome() {
+    $loader = new Twig\Loader\FilesystemLoader('view');
+    $twig = new Twig\Environment($loader);
+
+    // Charge le template 'Home.html.twig'
+    $template = $twig->load('Home.html.twig');
+    echo $template->render(array());
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -3,14 +3,19 @@
 require 'vendor/autoload.php';
 require_once 'models/model.php';
 require_once 'controleurs/Produits_controleur.php';
+require_once 'controleurs/Home_controleur.php';
 
+$homeController = new Home_controller();
 $productController = new Produits_controller();
 
 if(isset($_GET['action'])) {
   switch ($_GET['action']) {
-      case 'products':
-        $productController->afficherProduits();
+    case 'home':
+        $homeController->afficherHome();
         break;
+    case 'products':
+        $productController->afficherProduits();
+        break;    
     default:
             throw new Exception("Action non valide");
     }
