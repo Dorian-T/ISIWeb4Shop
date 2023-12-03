7b11d442f6716a7cc05db459ce559b48bb0f9c52
JulesUSG15 <jules.ginhac@etu.univ-lyon1.fr>
2023-12-03 16:22:31 +0100

    L'utilisateur peut payer en cb ou par virement

--- controller/Payement_controller.php
+++ controller/Payement_controller.php
@@ -12,10 +12,10 @@ class Payement_controller {
 
         if (!empty($_POST)) {
             $_SESSION['statut'] = 2;
-            if(isset($_SESSION['customer_id'])){
+            if(isset($_SESSION['customer_id'], $_SESSION['SESS_ORDERNUM'], $_SESSION['adresse_id'], $_SESSION['total'])){
                 $this->payement_model->updateOrder($_SESSION['SESS_ORDERNUM'], $_SESSION['customer_id'], 1, $_SESSION['adresse_id'], 1, 2, $_SESSION['total'], '');
             }
-            else{
+            else if(isset($_SESSION['SESS_ORDERNUM'], $_SESSION['adresse_id'], $_SESSION['total'])){
                 $this->payement_model->updateOrder($_SESSION['SESS_ORDERNUM'], 0, 0, $_SESSION['adresse_id'], 1, 2, $_SESSION['total'], session_id());
             }
 
@@ -23,14 +23,22 @@ class Payement_controller {
                 case 'paypal':
                     header('Location: https://www.paypal.com/fr/home');
                     break;
-                case 'card':
-                    header('Location: index.php?action=card');
+                case 'cb':
+                    $loader = new Twig\Loader\FilesystemLoader('view');
+                    $twig = new Twig\Environment($loader);
+                    $template = $twig->load('cb.twig');
+                    echo $template->render(array());
+                    exit();
                     break;
                 case 'cheque':
                     header('Location: facturePDF.php');
                     break;
                 case 'virement':
-                    header('Location: index.php?action=virement');
+                    $loader = new Twig\Loader\FilesystemLoader('view');
+                    $twig = new Twig\Environment($loader);
+                    $template = $twig->load('virement.twig');
+                    echo $template->render(array());
+                    exit();
                     break;
                 default:
                     header('Location: index.php?action=home');
